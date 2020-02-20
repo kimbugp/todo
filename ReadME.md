ea0274b85b605e68e7e85d3a8d552fdd203bd193
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2020-02-20 19:23:17 +0300

    initial commit

---
+++ ReadME.md
@@ -0,0 +1,5 @@
+# todo
+
+You can also spin up a free Heroku dyno to test it out:
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/kimbugp/todo)
---
+++ index.js
@@ -0,0 +1,29 @@
+const path = require("path");
+let express = require("express");
+
+var app = express();
+let PORT = 8000;
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+let DIST_DIR = path.join(__dirname, "src");
+
+app.use(express.static(DIST_DIR));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(DIST_DIR, "index.html"));
+});
+
+const reloader = () => {
+  io.on("connection", () => {
+    // io.sockets.emit("reload", {});
+  });
+  io.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+};
+
+reloader();
+server.listen(PORT, () => {
+  console.log(`go to http://localhost:${PORT}`);
+});
